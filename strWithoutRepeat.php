2d772eb1d40cd3774b86010b248ff91f92097d72
Lira <liramail96@gmail.com>
2019-02-28 13:19:50 +0200

    string without repeat

---
+++ strWithoutRepeat.php
@@ -0,0 +1,23 @@
+<?php
+function repeat($str)
+{
+    $a = '';
+    $b = [];
+    $arr = str_split($str);
+    foreach ($arr as $key => $value) {
+        if ((!strstr($a, $value))) {
+            $a .= $value;
+        }
+    }
+    for($j = strlen($a)-1; $j>=0; $j--){
+        if(!strstr($str, $a)){
+            $a = str_replace($a[$j], '', $a);
+        }
+    }
+    for($i = 0; $i < substr_count($str, $a); $i++){
+         $b[] = $a;
+     };
+
+    return $b;
+}
+var_dump(repeat('abcbcab'));
